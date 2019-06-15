4bde7b3f95d4a14c9b586b48dafc7e6f3e8ab6af
James-Alex Matthews <vamtiger@vamtiger.com>
2019-06-15 18:47:03 +0200

    test

--- build/test/browser.js
+++ build/test/browser.js
@@ -1,2 +1,37 @@
-!function(t,e){"use strict";function n(t,e,n,i){return new(n||(n=Promise))(function(o,c){function r(t){try{f(i.next(t))}catch(t){c(t)}}function u(t){try{f(i.throw(t))}catch(t){c(t)}}function f(t){t.done?o(t.value):new n(function(e){e(t.value)}).then(r,u)}f((i=i.apply(t,e)).next())})}const{VamtigerBrowserMethod:i}=window,{loadScript:o}=i;e.setup("bdd"),(()=>describe("vamtiger-cover-figure",function(){this.timeout(6e4),before(function(){return n(this,void 0,void 0,function*(){yield o({src:"vamtiger-cover-figure.js"})})}),it("load script",function(){return n(this,void 0,void 0,function*(){const e=document.head.querySelector('[src="vamtiger-cover-figure.js"]');t.expect(e instanceof HTMLScriptElement).to.be.true})})}))(),e.run()}(chai,mocha);
+(function (chai,mocha) {
+'use strict';
+
+function __awaiter(thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+}
+
+const { VamtigerBrowserMethod } = window;
+const { loadScript } = VamtigerBrowserMethod;
+var loadScript$1 = () => describe('vamtiger-cover-figure', function () {
+    this.timeout(60000);
+    before(function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield loadScript({
+                src: 'vamtiger-cover-figure.js'
+            });
+        });
+    });
+    it('load script', function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            const script = document.head.querySelector('[src="vamtiger-cover-figure.js"]');
+            chai.expect(script instanceof HTMLScriptElement).to.be.true;
+        });
+    });
+});
+
+mocha.setup('bdd');
+loadScript$1();
+mocha.run();
+
+}(chai,mocha));
 //# sourceMappingURL=browser.js.map
--- build/test/node.js
+++ build/test/node.js
@@ -1,2 +1,54 @@
-"use strict";function _interopDefault(t){return t&&"object"==typeof t&&"default"in t?t.default:t}var path=require("path"),chai=require("chai"),copyFile=_interopDefault(require("vamtiger-copy-file")),getFolderContent=_interopDefault(require("vamtiger-get-directory-content"));function __awaiter(t,e,i,n){return new(i||(i=Promise))(function(o,r){function s(t){try{l(n.next(t))}catch(t){r(t)}}function a(t){try{l(n.throw(t))}catch(t){r(t)}}function l(t){t.done?o(t.value):new i(function(e){e(t.value)}).then(s,a)}l((n=n.apply(t,e)).next())})}require("tslib");const buildFolder=path.resolve(__dirname,".."),testHtmlSource=path.resolve(__dirname,"../../source/test/html/test.html"),testHtmlDestination=path.resolve(__dirname,"../index.html"),testHtmlTemplateSource=path.resolve(__dirname,"../../source/test/html/template.svg"),testHtmlTemplateDestination=path.resolve(__dirname,"../template.svg"),testCssSource=path.resolve(__dirname,"../../source/test/html/test.css"),testCssDestination=path.resolve(__dirname,"../test.css");describe("Generate",function(){before(function(){return __awaiter(this,void 0,void 0,function*(){yield Promise.all([copyFile({source:testHtmlSource,destination:testHtmlDestination}),copyFile({source:testCssSource,destination:testCssDestination}),copyFile({source:testHtmlTemplateSource,destination:testHtmlTemplateDestination})])})}),it("Test Preview",function(){return __awaiter(this,void 0,void 0,function*(){const t=yield getFolderContent(buildFolder).then(t=>new Set(t));chai.expect(t.has(path.basename(testHtmlDestination))).to.be.true})})});
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var path = require('path');
+var chai = require('chai');
+var copyFile = _interopDefault(require('vamtiger-copy-file'));
+var getFolderContent = _interopDefault(require('vamtiger-get-directory-content'));
+require('tslib');
+
+function __awaiter(thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+}
+
+const buildFolder = path.resolve(__dirname, '..');
+const testHtmlSource = path.resolve(__dirname, '../../source/test/html/test.html');
+const testHtmlDestination = path.resolve(__dirname, '../index.html');
+const testHtmlTemplateSource = path.resolve(__dirname, '../../source/test/html/template.svg');
+const testHtmlTemplateDestination = path.resolve(__dirname, '../template.svg');
+const testCssSource = path.resolve(__dirname, '../../source/test/html/test.css');
+const testCssDestination = path.resolve(__dirname, '../test.css');
+describe('Generate', function () {
+    before(function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield Promise.all([
+                copyFile({
+                    source: testHtmlSource,
+                    destination: testHtmlDestination
+                }),
+                copyFile({
+                    source: testCssSource,
+                    destination: testCssDestination
+                }),
+                copyFile({
+                    source: testHtmlTemplateSource,
+                    destination: testHtmlTemplateDestination
+                })
+            ]);
+        });
+    });
+    it('Test Preview', function () {
+        return __awaiter(this, void 0, void 0, function* () {
+            const folderContent = yield getFolderContent(buildFolder)
+                .then(result => new Set(result));
+            chai.expect(folderContent.has(path.basename(testHtmlDestination))).to.be.true;
+        });
+    });
+});
 //# sourceMappingURL=node.js.map
