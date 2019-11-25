f83172eb8fc53fe248f39f0cdf8a62454d989c93
James-Alex Matthews <vamtiger@vamtiger.com>
2019-11-25 23:37:19 +0200

    Saving

--- build/element.d.ts
+++ build/element.d.ts
@@ -2,7 +2,7 @@ import { ObservedAttributes } from './types';
 export declare const name = "vamtiger-cover-figure";
 export default class VamtigerCoverFigure extends HTMLElement {
     constructor();
-    static readonly observedAttributes: ObservedAttributes[];
+    static get observedAttributes(): ObservedAttributes[];
     connectedCallback(): Promise<void>;
     attributeChangedCallback(name: ObservedAttributes, oldValue: string, newValue: string): void;
 }
